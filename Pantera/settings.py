b78f61f1b0f21923723068beb91109d43e1925bd
nuel <omoragbonemmanuel@gmail.com>
2022-10-29 11:08:32 -0400

    added pipreqs to generate requirements.txt file

--- Pantera/settings.py
+++ Pantera/settings.py
@@ -126,7 +126,7 @@ STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [BASE_DIR.joinpath('static')]
 
-STATIC_ROOT = '/var/www/pantera/static'
+STATIC_ROOT = '/home/pantera/pantera.pythonanywhere.com/static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
