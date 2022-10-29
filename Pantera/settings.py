36e047b3cd326fe72859501f5ed5ac8cf1f21860
nuel <omoragbonemmanuel@gmail.com>
2022-10-29 11:39:52 -0400

    configured settings.py for production

--- Pantera/settings.py
+++ Pantera/settings.py
@@ -125,8 +125,6 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [BASE_DIR.joinpath('static')]
-
 STATIC_ROOT = '/home/pantera/pantera.pythonanywhere.com/static'
 
 # Default primary key field type
